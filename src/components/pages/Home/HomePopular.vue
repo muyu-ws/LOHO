4a203b3419a9557167912d242b3d0161f27988d0
734840323@qq.com <734840323@qq.com>
2018-08-02 22:17:58 +0800

    ywslll

---
+++ src/components/pages/Home/HomePopular.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="app-home-popular">
+    <div class="popular" >
+        <div class="title">
+            <h2>人气推荐</h2>
+            <a href="http://www.loho88.com/m/activ.php?id=250">更多></a>
+        </div>
+        <img src="http://img.loho88.com/images/2018/zssxydd.jpg">
+        <ul class="popular-list">
+            <li v-for="popular in populars" :key="popular.gid">
+                <span>{{popular.tag}}</span>
+                <p>{{popular.tag_en}}</p>
+                <img :src="popular.pic" alt="">
+            </li>
+        </ul>
+        </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "app-home-popular",
+  data() {
+    return {
+      populars: []
+    };
+  },
+  methods: {
+    getPopular() {
+      this.$http.get("/lh/index").then(res => {
+        this.populars = res.data.result.popular.show;
+         console.log(this.populars);
+      });
+    }
+  },
+  created() {
+    this.getPopular();
+  }
+};
+</script>
+<style scoped lang = "scss" scoped>
+
+ .popular{
+     height: 6.85rem;
+     margin-top:0.1rem;
+
+     .title{
+         display: flex;
+         width:100%;
+         flex-direction: row;
+         height:0.42rem;
+         justify-content:space-between;
+         background-color: #fff;
+         text-align: center;
+
+         h2{
+             line-height: 0.22rem;
+             padding: 0.1rem;
+         }
+         a{
+             line-height: 0.22rem;
+             padding: 0.1rem;
+             color:rgb(236, 13, 13);
+         }
+     }
+     img{
+        display: inline-block;
+        vertical-align: middle;
+        width: 100%;
+        height: 1rem;
+     }
+
+     .popular-list{
+         display: flex;
+         flex-wrap: wrap;
+
+         li{
+            height: 1.56rem;
+            flex-wrap: wrap;
+            flex-flow: column;
+            align-items: center;
+            justify-content: space-between;
+             display: flex;
+             width: 50%;
+             padding: 0.12rem;
+             background: #fff;
+
+             span{
+                    height: 40px;
+                    line-height: 20px;
+                    width: 110%;
+                    text-align: center;
+
+             }
+             p{
+                line-height: 0.12rem;
+                font-size: 0.14rem;
+                height: 0.52rem;
+                width: 100%;
+                text-align: center;
+                padding: 0 0.15rem;
+                color: #ccc;
+             }
+
+             img{
+                height: 0.65rem;
+                 width: 100%;
+             }
+         }
+     }
+ }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-
-export default new VueRouter({
-    routers: [
+import Home from "../components/pages/Home/Home";
+const routes =  [
         {
             path: '/',
-            name: ''
+            name: 'home',
+            component:Home
         }
     ]
+const router = new VueRouter({
+    mode:'history',
+    routes
 })
+
+export default router
