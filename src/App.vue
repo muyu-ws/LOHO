0b9a5ca29425406de555781ff7570b784ba31c4e
734840323@qq.com <734840323@qq.com>
2018-08-03 09:07:31 +0800

    改了接口

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="app">
-    <app-home></app-home>
+    <router-view></router-view>
+    <!-- <app-home></app-home> -->
   </div>
 </template>
 <script>
-import AppHome from "./components/pages/Home/Home";
+
 export default {
-  name: "App",
-  components: {
-    AppHome: AppHome
-  }
+  name: "App"
+ 
 };
 </script>
 
